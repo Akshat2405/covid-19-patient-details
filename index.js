2a624dbd12fe3ded229bb62a98b685f79da9128d
Akshat Jain <akshat87001@gmail.com>
2020-08-28 16:01:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("This is my new project");
+
+
+function updatecases(){
+    fetch('https://api.covid19india.org/data.json')
+    .then(response=> response.text())
+    .then((text) =>{
+      const textobj=JSON.parse(text);
+      const myiid=document.getElementById("myiid");
+      const myobj=textobj.statewise
+      let string=""
+      for (const item of myobj) {
+          string += `<tr>
+          <th scope="row">${item.state}</th>
+          <td>${item.confirmed}</td>
+          <td>${item.recovered}</td>
+          <td>${item.active}</td>
+          <td>${item.deaths}</td>
+        </tr>`
+      }
+   
+   
+      myiid.innerHTML=string;
+    });
+}
+updatecases();
+setInterval(()=>{
+    console.log("ohh");
+    updatecases()},3000   
+)
